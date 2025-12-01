4a059e1b80cae532ace54a746938d5b569d7c985
VitriolOlja <148021673+VitriolOlja@users.noreply.github.com>
2025-12-01 11:15:10 +0100

    2025 day 1

---
+++ 2025/1/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main()  {
+  
+  int16_t dial = 50;
+
+  FILE *fptr = fopen("input.txt", "r");
+  char buffer[10];
+  uint16_t count = 0;
+  while (fgets(buffer, sizeof(buffer), fptr)) {
+    //printf("%s", buffer); 
+    int num = atoi(&buffer[1]);
+    if (buffer[0] == 'L') {
+      dial -= num;
+    }
+    else {
+      dial += num;
+    }
+    dial = ((dial % 100) + 100) % 100;
+    if (dial == 0) {
+      count++;
+    }
+  }
+
+  printf("Count: %d\n", count);
+ 
+  
+
+  return 0;
+}
---
+++ 2025/2/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main()  {
+  
+  int16_t dial = 50;
+  int16_t prev_dial;
+
+  FILE *fptr = fopen("input.txt", "r");
+  char buffer[10];
+  uint16_t count = 0;
+  while (fgets(buffer, sizeof(buffer), fptr)) {
+    //printf("dial: %d, count: %d, instr: %s", dial, count, buffer);
+    //printf("%s", buffer); 
+    int num = atoi(&buffer[1]);
+    count += num / 100;
+    num %= 100;
+    int prev_dial = dial;
+    if (buffer[0] == 'L') {
+      dial -= num;
+    }
+    else {
+      dial += num;
+    }
+    
+    if (dial < 0) {
+      dial += 100;
+      if (prev_dial != 0){
+        count++;
+      }
+    }
+    else if (dial >= 100) {
+      dial -= 100;
+      if (prev_dial != 0){
+        count++;
+      }
+    }
+    else if (dial == 0) { 
+      count++;
+    }
+  }
+  
+  //printf("\ndial: %d, count: %d, instr: %s\n", dial, count, buffer);
+
+  printf("Count: %d\n", count);
+
+  return 0;
+}
